8a11536e7067d7351e7a993d7ab3321229924c27
May <mayar_234@outlook.com>
2025-09-17 22:19:02 -0300

    feat: ajuste nav

---
+++ assets/JS/regiõesnav.js
@@ -0,0 +1,7 @@
+  function exibe_regioes() {
+    document.getElementById("sub_menu").style.display = "block";
+  }
+
+  function some_regioes() {
+    document.getElementById("sub_menu").style.display = "none";
+  }
